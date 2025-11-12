23c0e799c260ad2c552faf40db44f2011381cd1c
Maaz Amir <amirmaaz16@gmail.com>
2025-11-13 02:02:10 +0530

    Implement chat functionality using readline-sync and update dependencies

--- LLM.js
+++ LLM.js
@@ -1,19 +1,38 @@
 import { GoogleGenAI } from "@google/genai";
 import {} from "dotenv/config.js";
+import readlineSync from "readline-sync";
 
 const ai = new GoogleGenAI({ apiKey: process.env.Gemini_Api_Key });
 
-async function main() {
+const History = [];
+
+async function Chatting(userProblem) {
+  History.push({
+    role: "user",
+    parts: [{ text: userProblem }],
+  });
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: [
-      {
-        role: "user",
-        parts: [{ text: "Hi, I am Maaz" }],
-      },
-    ],
+    // contents: [
+    //   {
+    //     role: "user",
+    //     parts: [{ text: "Whats the weather in Bhopal?" }],
+    //   },
+    // ],
+    contents: History,
   });
+
+  History.push({
+    role: "model",
+    parts: [{ text: response.text }],
+  });
+
   console.log(response.text);
 }
 
+async function main() {
+  const userProblem = readlineSync.question("Ask me anything--> ");
+  await Chatting(userProblem);
+  main();
+}
 await main();
